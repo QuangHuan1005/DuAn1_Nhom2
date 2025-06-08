86ee28dae015b78132b8560381a0128c11fbb1e7
QuangHuan1005 <quanghuanph52249@gmail.com>
2025-06-08 13:01:06 +0700

    Cập nhật giỏ hàng

--- views/product/product-detail.php
+++ views/product/product-detail.php
@@ -9,7 +9,9 @@
     <?php endif; ?>
 
     <div class="container margin_30">
-        <div class="countdown_inner">-20% Ưu đãi này kết thúc sau <div data-countdown="2025/05/25" class="countdown"></div></div>
+        <div class="countdown_inner">
+            -20% Ưu đãi này kết thúc sau <div data-countdown="2025/05/25" class="countdown"></div>
+        </div>
 
         <?php if ($product): ?>
         <div class="row">
@@ -17,7 +19,7 @@
             <div class="col-md-6">
                 <div class="all">
                     <div class="slider">
-                        <img src="<?= $product['image_url'] ?>" alt="<?= $product['name'] ?>">
+                        <img src="<?= htmlspecialchars($product['image_url']) ?>" alt="<?= htmlspecialchars($product['name']) ?>">
                     </div>
                 </div>
             </div>
@@ -28,18 +30,15 @@
                     <ul>
                         <li><a href="index.php">Trang chủ</a></li>
                         <li><a href="#">Danh mục</a></li>
-                        <li><?= $product['name'] ?></li>
+                        <li><?= htmlspecialchars($product['name']) ?></li>
                     </ul>
                 </div>
 
                 <div class="prod_info">
-                    <h1><?= ($product['name']) ?></h1>
+                    <h1><?= htmlspecialchars($product['name']) ?></h1>
                     <span class="rating"><em>Tồn kho:
                             <?= isset($product['stock_quantity']) ? (int) $product['stock_quantity'] : 'Không có thông tin' ?></em></span>
-                    <p><small>SKU: MTKRY-00<?= ($product['id']) ?></small>
-                        <!-- <br>Sed ex labitur adolescens scriptorem. Te
-                        saepe verear tibique sed. Et wisi ridens vix, lorem iudico blandit mel cu. Ex vel sint zril
-                        oportere, amet wisi aperiri te cum.</p> -->
+                    <p><small>SKU: MTKRY-00<?= (int)$product['id'] ?></small></p>
 
                     <div class="prod_options">
                         <form action="./?act=cart/add" method="post">
@@ -49,46 +48,35 @@
                                     <input type="number" name="quantity" value="1" min="1" class="form-control" style="width: 100px;" required>
                                 </div>
                             </div>
-                            <div class="row align-items-center">
+
+                            <div class="row align-items-center mb-3">
                                 <div class="col-lg-5 col-md-6 mb-2">
                                     <div class="price_main">
-                                        <span class="new_price"><?= number_format($product['discount_price']) ?>₫</span>
-                                        <span class="percentaged"></span>
-                                        <span class="old_price"><?= number_format($product['price']) ?>₫</span>
+                                        <?php
+                                            $original = $product['price'] ?? 0;
+                                            $discount = $product['discount_price'] ?? $original;
+                                            $percent = 0;
+
+                                            if ($original > 0 && $discount < $original) {
+                                                $percent = round((($original - $discount) / $original) * 100);
+                                            }
+                                        ?>
+                                        <span class="new_price"><?= number_format($discount, 0, ',', '.') ?>₫</span>
+                                        <?php if ($percent > 0): ?>
+                                            <span class="percentaged text-danger">-<?= $percent ?>%</span>
+                                        <?php endif; ?>
+                                        <?php if ($percent > 0): ?>
+                                            <span class="old_price"><?= number_format($original, 0, ',', '.') ?>₫</span>
+                                        <?php endif; ?>
+                                    </div>
+                                    <div class="stock_quantity mt-2">
+                                        <strong>Số lượng tồn kho: </strong>
+                                        <?= isset($product['stock_quantity']) ? (int)$product['stock_quantity'] : 'Không có thông tin' ?>
                                     </div>
                                 </div>
 
-
-        <div class="row align-items-center">
-           <div class="col-lg-5 col-md-6 mb-2">
-    <div class="price_main">
-        <span class="new_price"><?= number_format($product['discount_price'] ?? 0, 0, ',', '.') ?>₫</span>
-
-<?php
-    $original = $product['price'] ?? 0;
-    $discount = $product['discount_price'] ?? 0;
-    $percent = 0;
-
-    if ($original > 0 && $discount < $original) {
-        $percent = round((($original - $discount) / $original) * 100);
-    }
-?>
-
-<?php if ($percent > 0): ?>
-    <span class="percentaged text-danger">-<?= $percent ?>%</span>
-<?php endif; ?>
-
-<span class="old_price"><?= number_format($product['price'] ?? 0, 0, ',', '.') ?>₫</span>
-
-    </div>
-   
-    <div class="stock_quantity mt-2">
-        <strong>Số lượng tồn kho: </strong>
-        <?= isset($product['stock_quantity']) ? (int)$product['stock_quantity'] : 'Không có thông tin' ?>
-    </div>
-</div>
                                 <div class="col-lg-4 col-md-6 mb-2">
-                                    <input type="hidden" name="product_id" value="<?= $product['id'] ?>">
+                                    <input type="hidden" name="product_id" value="<?= (int)$product['id'] ?>">
                                     <button type="submit" class="btn_1" title="Thêm vào giỏ hàng">
                                         <i class="ti-shopping-cart"></i> Thêm vào giỏ hàng
                                     </button>
@@ -128,7 +116,7 @@
                         <div class="row justify-content-between">
                             <div class="col-lg-12">
                                 <h3>Chi tiết sản phẩm</h3>
-                                <p><?= nl2br($product['description']) ?></p>
+                                <p><?= nl2br(htmlspecialchars($product['description'])) ?></p>
                             </div>
                         </div>
                     </div>
@@ -163,9 +151,9 @@
                                 <h3>Bình luận</h3>
                                 <?php if (isset($_SESSION['user'])): ?>
                                     <form method="post" action="index.php?act=add_comment">
-                                        <input type="hidden" name="product_id" value="<?= $product['id'] ?>">
+                                        <input type="hidden" name="product_id" value="<?= (int)$product['id'] ?>">
                                         <textarea name="content" rows="4" cols="50" required placeholder="Nhập bình luận..."></textarea><br>
-                                        <button type="submit">Gửi bình luận</button>
+                                        <button type="submit" class="btn btn-primary">Gửi bình luận</button>
                                     </form>
                                 <?php else: ?>
                                     <p>Vui lòng <a href="index.php?act=login">đăng nhập</a> để bình luận.</p>
@@ -192,7 +180,7 @@
                     <span class="ribbon new">New</span>
                     <figure>
                         <a href="product-detail-1.html">
-                            <img class="owl-lazy" src="img/products/product_placeholder_square_medium.jpg" alt="">
+                            <img class="owl-lazy" src="img/products/product_placeholder_square_medium.jpg" alt="Sản phẩm mẫu">
                         </a>
                     </figure>
                     <div class="rating">
